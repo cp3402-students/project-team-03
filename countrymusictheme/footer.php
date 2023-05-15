d9ecb55e014195cdf3a0384c63ed30d1be4769a4
isaacbarry1 <114851668+isaacbarry1@users.noreply.github.com>
2023-05-15 22:29:11 +1000

    Add colour and basic formatting
    Add padding to the theme as well as colour that can be changed at a later date.

--- countrymusictheme/footer.php
+++ countrymusictheme/footer.php
@@ -16,13 +16,13 @@
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'countrymusictheme' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'countrymusictheme' ), 'WordPress' );
+				printf( esc_html__( 'Townsville Jazz Club' ), 'WordPress' );
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'countrymusictheme' ), 'countrymusictheme', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Townsville Jazz Club'));
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->

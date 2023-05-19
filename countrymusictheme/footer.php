702770a6ec97307da1da4a147c3f4731121848f5
OliverWilsonHolland <69840392+OliverWilsonHolland@users.noreply.github.com>
2023-05-19 22:56:38 +1000

    Updated Footer.php and Style.css
    Incorporated dynamic widgets in the footer.
    Added styling for the new section in the footer.

--- countrymusictheme/footer.php
+++ countrymusictheme/footer.php
@@ -10,22 +10,18 @@
  */
 
 ?>
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'countrymusictheme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Powered By Wordpress' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				bloginfo( 'name'); ;
-				?>
-		</div><!-- .site-info -->
+	<footer id="colophon" class="site-footer-top">
+		<div class="site-footer-coloumn">
+			<?php dynamic_sidebar('footer-section-1'); ?>
+		</div><!-- .site-footer-coloumn -->
+		<div class="site-footer-coloumn">
+			<?php dynamic_sidebar('footer-section-2'); ?>
+		</div><!-- .site-footer-coloumn -->
+		<div class="site-footer-coloumn">
+			<?php dynamic_sidebar('footer-section-3'); ?>
+		</div><!-- .site-footer-coloumn -->
 	</footer><!-- #colophon -->
-	<footer id="colophon" class="site-footer">
+	<footer id="colophon" class="site-footer-bottom">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'countrymusictheme' ) ); ?>">
 				<?php
